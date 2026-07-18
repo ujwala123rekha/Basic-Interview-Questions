35c3bbeb42f4236f298e2df8683250f45c65a633
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-07-18 23:04:27 +0530

    Create PrimeNumber7.java

---
+++ PrimeNumber7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 1) {
+            System.out.println("Not Prime");
+            return;
+        }
+        boolean prime = true;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
