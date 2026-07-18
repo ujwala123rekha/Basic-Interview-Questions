e2faf6ba36b84caafe12eccabea978516f36ec34
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-07-18 23:05:27 +0530

    Implement LCM calculation using GCD

---
+++ LCM9.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+
+        int lcm = (a * b) / gcd;
+
+        System.out.println(lcm);
+    }
+}
