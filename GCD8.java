d0e47c0f4ca4ace6fb1b0af29b8ff6f5109c4a1c
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-07-18 23:04:57 +0530

    Add GCD calculation in GCD8.java

---
+++ GCD8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println(a);
+    }
+}
